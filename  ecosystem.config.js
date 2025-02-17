f942e03646a4e541419f9d1773d7004b1698082a
ogiste <alfred.opon@strathmore.edu>
2025-02-17 20:58:18 +0300

    refactor(config): rename app and update path
    - renamed app from "open-scroll" to "scroll-open"
    - updated working directory path accordingly
    
    this change helps maintain naming consistency across the project. no functional changes made, so app behavior should remain unchanged.
    
    todo:
    - check for references in documentation and scripts
    - update deployment pipelines if necessary

---  ecosystem.config.js
+++  ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "open-scroll", // Name of your app in PM2
-      cwd: "/var/www/open-scroll", // Working directory
+      name: "scroll-open", // Name of your app in PM2
+      cwd: "/var/www/scroll-open", // Working directory
       script: "yarn", // Command to run
       args: "start", // Arguments for the command
       env: {
