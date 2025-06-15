5188c8bd3e993d244ea7fe1036492914bb9f5b30
ogiste <alfred.opon@strathmore.edu>
2025-06-16 01:00:05 +0300

    feat(web3): enhance Web3 integration and user authentication
    - Updated useWeb3 hook to create AppKit instance with improved metadata and theme variables.
    - Refactored user service to include new SIWE endpoints for nonce retrieval and signature verification.
    - Introduced methods for getting nonce and verifying signature, streamlining Web3 authentication processes.
    - Improved code organization and readability across the affected files.

--- src/hooks/useWeb3.ts
+++ src/hooks/useWeb3.ts
@@ -1,37 +1,41 @@
 import { ethersAdapter, networks, projectId } from "@/config/web3";
 
 import colors from "tailwindcss/colors";
-import { createAppKit } from "@reown/appkit/react";
+import { createAppKit, useAppKit } from "@reown/appkit/react";
 
 export const useWeb3 = () => {
   // 2. Create a metadata object
   const metadata = {
     name: "Sasasasa",
     description: "Community to Commerce, now now",
-    url: process.env.BASE_PATH || "https://beta.sasasasa.co", // origin must match your domain & subdomain
+    url: process.env.NEXT_PUBLIC_APP_URL || "https://beta.sasasasa.co", // origin must match your domain & subdomain
     icons: ["https://beta.sasasasa.co/images/sasasasaLogo.png"],
   };
 
+  // 3. Create the AppKit instance
+  createAppKit({
+    adapters: [ethersAdapter],
+    metadata,
+    networks,
+    projectId,
+    features: {
+      email: true, // default to true
+      socials: ["google", "github", "discord", "x", "farcaster"],
+      emailShowWallets: true, // default to true,
+      analytics: true,
+    },
+    themeVariables: {
+      "--w3m-color-mix": colors.pink[500],
+      "--w3m-color-mix-strength": 40,
+      "--w3m-accent": "#CC322D",
+    },
+    allWallets: "SHOW", // default to SHOW
+  });
+
+  const appKit = useAppKit();
+
   const initializeAppKitButton = () => {
-    // 3. Create the AppKit instance
-    createAppKit({
-      adapters: [ethersAdapter],
-      metadata,
-      networks,
-      projectId,
-      features: {
-        email: true, // default to true
-        socials: ["google", "github", "discord", "x", "farcaster"],
-        emailShowWallets: true, // default to true,
-        analytics: true,
-      },
-      themeVariables: {
-        "--w3m-color-mix": colors.pink[500],
-        "--w3m-color-mix-strength": 40,
-        "--w3m-accent": "#CC322D",
-      },
-      allWallets: "SHOW", // default to SHOW
-    });
+    appKit.open();
   };
 
   return {
